d0f212f8463643d61450309a5d47850b78519f45
Hoon94 <dleognsdl1@naver.com>
2025-08-11 22:44:26 +0900

    feat: 프로젝트 생성 및 필요 라이브러리 설치

---
+++ Diary/Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Daehoon Lee on 8/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
+    }
+
+
+}
+
