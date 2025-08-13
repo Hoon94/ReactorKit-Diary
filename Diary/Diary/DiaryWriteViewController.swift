4e56cb79b0186dafaf136fe2146f8ed9568dab2c
Hoon94 <dleognsdl1@naver.com>
2025-08-13 23:45:27 +0900

    feat: writeviewcontroller ui 추가

--- Diary/Diary/DiaryWriteViewController.swift
+++ Diary/Diary/DiaryWriteViewController.swift
@@ -5,16 +5,93 @@
 //  Created by Daehoon Lee on 8/12/25.
 //
 
+import SnapKit
 import UIKit
 
 final class DiaryWriteViewController: UIViewController {
     
+    private let titleLabel = {
+        let label = UILabel()
+        label.text = "제목"
+        return label
+    }()
+    
+    private let titleTextField = {
+        let textField = UITextField()
+        textField.borderStyle = .bezel
+        return textField
+    }()
+    
+    private let contentLabel = {
+        let label = UILabel()
+        label.text = "내용"
+        return label
+    }()
+    
+    private let contentTextView = {
+        let textView = UITextView()
+        textView.layer.borderColor = UIColor.systemGray.cgColor
+        textView.layer.borderWidth = 1
+        textView.font = .systemFont(ofSize: 16)
+        return textView
+    }()
+    
+    private let saveButton = {
+        let button = UIButton(configuration: .filled())
+        button.setTitle("저장", for: .normal)
+        return button
+    }()
+    
     init() {
         super.init(nibName: nil, bundle: nil)
-        view.backgroundColor = .purple
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        setUI()
+    }
+    
+    private func setUI() {
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        view.addSubview(titleTextField)
+        view.addSubview(contentLabel)
+        view.addSubview(contentTextView)
+        view.addSubview(saveButton)
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.leading.equalToSuperview().offset(16)
+        }
+        
+        titleTextField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(44)
+        }
+        
+        contentLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleTextField.snp.bottom).offset(4)
+            make.leading.equalTo(titleLabel)
+        }
+        
+        contentTextView.snp.makeConstraints { make in
+            make.top.equalTo(contentLabel.snp.bottom).offset(4)
+            make.leading.trailing.equalTo(titleTextField)
+            make.bottom.equalTo(saveButton.snp.top).offset(-20)
+        }
+        
+        saveButton.snp.makeConstraints { make in
+            make.height.equalTo(60)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
+        }
     }
 }
